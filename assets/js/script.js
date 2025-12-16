6010896a468d974b58bd3579b54ee21c600cb344
tolase <tolasedairo@gmail.com>
2025-12-16 20:34:57 +0000

    add event listner for document loading and button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+//wait for the DOM to finish loading
+//get the button elements and add event listeners to them
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("you clicked submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`you clicked ${gameType}`);
+            }
+        });
+    }
+});
+
 function runGame(){}
 function checkAnswer(){}
 function calculateCorrectAnswer(){}
