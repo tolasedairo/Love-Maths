8666e50d027755b2a2fdb362616701806fdc39fd
tolase <tolasedairo@gmail.com>
2025-12-16 22:08:05 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,27 +8,40 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("you clicked submit");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`you clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+    runGame("addition");
 });
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed 
  */
-function runGame(){
+function runGame(gameType){
     //generate two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else { 
+        alert(`Unknown game type: ${gameType}`);  
+        throw `unknown game type: ${gameType} aborting!`;
+    }
 
 }
 function checkAnswer(){}
 function calculateCorrectAnswer(){}
 function incrementScore(){}
 function incrementWrongAnswer(){}
-function displayAdditionQuestion(){}
+
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById("operand1").innerText = operand1;
+    document.getElementById("operand2").innerText = operand2;
+    document.getElementById("operator").innerText = "+";
+}
+
 function displaySubtractQuestion(){}
 function displayMultiplyQuestion(){}
 function displayDivisionQuestion(){}
