a6e2553c60463215b0cba78137137b128a59ebfe
tolase <tolasedairo@gmail.com>
2025-12-16 17:20:31 +0000

    add structure for game function

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function runGame(){}
+function checkAnswer(){}
+function calculateCorrectAnswer(){}
+function incrementScore(){}
+function incrementWrongAnswer(){}
+function displayAdditionQuestion(){}
+function displaySubtractQuestion(){}
+function displayMultiplyQuestion(){}
+function displayDivisionQuestion(){}
