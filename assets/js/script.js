5cdd000adfb890a1e8729d2f8d51602898bd05e0
tolase <tolasedairo@gmail.com>
2025-12-17 17:27:07 +0000

    add subtract and division code to the game, favicon and listen to key press

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,14 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    // listen for enter key press
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
+
+    // Start the game for the first time
     runGame("addition");
 
 });
@@ -24,6 +32,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+    // Clears the answer box and focuses it
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
@@ -33,7 +44,11 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if(gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-    } else {
+    } else if(gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    } else if(gameType === "division") {
+        displayDivisionQuestion(num1, num2);
+    } else {    
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -76,6 +91,10 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if(operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if(operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    } else if(operator === "÷") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -111,12 +130,20 @@ function displayAdditionQuestion(operand1, operand2) {
     
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "÷";
 }
